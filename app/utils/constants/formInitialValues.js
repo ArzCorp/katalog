157d80eac0440f55355613a20c3c9dd96d270d75
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2023-09-03 22:25:35 -0600

    ✨ add form initial values constants

---
+++ app/utils/constants/formInitialValues.js
@@ -0,0 +1,14 @@
+export const FORM_INITIAL_VALUES = Object.freeze({
+  REGISTER: {
+    name: '',
+    lastName: '',
+    email: '',
+    catalogName: '',
+    password: '',
+    repeatedPassword: '',
+  },
+  LOGIN: {
+    email: '',
+    password: '',
+  },
+})
