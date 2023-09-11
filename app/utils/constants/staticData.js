5cc21c6d5bff58f50767e57c9dceba8e11153c31
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2023-09-10 20:44:48 -0600

    ✨ add static data constants

---
+++ app/utils/constants/staticData.js
@@ -0,0 +1,17 @@
+export const NAVBAR_ELEMENTS = [
+  {
+    name: 'Agregar producto',
+    href: '',
+    id: 1,
+  },
+  {
+    name: 'Compartir',
+    href: '',
+    id: 2,
+  },
+  {
+    name: 'Ajustes',
+    href: '',
+    id: 3,
+  },
+]
